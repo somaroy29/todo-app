af463dc67c246fba51d76e04e96f3e6e0b9634c8
Soma Roy <68267878+somaroy29@users.noreply.github.com>
2022-01-12 02:05:30 +0530

    simple-JS-form

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
-const msg=document.getElementById("message");
+var myform=document.forms.newform;
+var message=document.getElementById("message");
 
-function myMsg(){
-    msg.className="hidden";
-}
-setTimeout(myMsg,3000);
+myform.onsubmit=function(){
+    if(myform.name.value.trim()===''){
+        message.innerHTML="please enter your name";
+        return false;
+    }
+    else{
+        message.innerHTML="";
+        return true;
+    }
+};
+
+
+
+
+
+//blur function
+/*myform2.name.onblur=function(){
+    myform2.name.style.border="none";
+}*/
