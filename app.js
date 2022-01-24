fe864d3401d1ba5fa3918456b5c6e0e758b0d68a
Soma Roy <68267878+somaroy29@users.noreply.github.com>
2022-01-24 17:50:44 +0530

    modal-with-JS

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-//  method 1 -> traversing the DOM 
-//=====================================================================
-// const btns = document.querySelectorAll(".question-btn");
-// btns.forEach(function(btn){
-// btn.addEventListener("click",function(e){
-//     const question =e.currentTarget.parentElement.parentElement;
-//     question.classList.toggle("show-text");
-// })
-// })
+const modalBtn= document.querySelector(".modal-btn");
+const modalOverlay=document.querySelector(".modal-overlay");
+const closeBtn=document.querySelector(".close-btn");
 
-//  method 2 -> using selectors inside the element
-//=====================================================================
-const questions = document.querySelectorAll(".question")
-questions.forEach(function(question){
-   const btn=question.querySelector(".question-btn")
-   btn.addEventListener("click",function(){
-       questions.forEach(function(item){
-           if(item !== question){
-               item.classList.remove("show-text")
-           }
-       })
-       question.classList.toggle("show-text");
-   });
+modalBtn.addEventListener("click",function(){
+    modalOverlay.classList.add("open-modal");
 });
+
+closeBtn.addEventListener("click",function(){
+    modalOverlay.classList.remove("open-modal");
+});
+
