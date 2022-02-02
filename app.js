7a13b7a5292f70fa1eb6b50327bcc7f380cba370
Soma Roy <68267878+somaroy29@users.noreply.github.com>
2022-02-02 22:31:17 +0530

    changing-content-via-TAB-using-JS

--- app.js
+++ app.js
@@ -1,44 +1,21 @@
-const date=document.getElementById("date")
-const navToggle=document.querySelector(".nav-toggle")
-const linksContainer=document.querySelector(".links-container")
-const links=document.querySelector(".links")
+const about=document.querySelector(".about")
+const btns=document.querySelectorAll(".tab-btn")
+const articles=document.querySelectorAll(".content")
 
-navToggle.addEventListener("click",function(){
-    // linksContainer.classList.toggle("show-links");
-    const containerHeight=linksContainer.getBoundingClientRect().height;
-    // console.log(containerHeight);
-    const linksHeight=links.getBoundingClientRect().height;
-    // console.log(linksHeight)
-    if(containerHeight === 0){
-        linksContainer.style.height=`${linksHeight}px`;
-    }
-    else{
-        linksContainer.style.height=0;
-    }
+about.addEventListener("click",function(e){
+   const id=e.target.dataset.id;
+   if(id){
+//removing active from other button
+       btns.forEach(function(btn){
+           btn.classList.remove("active");
+           e.target.classList.add("active");
+       });
+       //hide other articles
+       articles.forEach(function(article){
+           article.classList.remove("active")
+       })
+       const element=document.getElementById(id);
+       element.classList.add("active");
+       console.log(element)
+   }
 });
-
-//***************** FIXED NAVBAR ********************//
-const navbar=document.getElementById("nav");
-const topLink=document.querySelector(".top-link");
-
-window.addEventListener("scroll",function(){
-    const scrollHeight=window.pageYOffset;
-    const navHeight=navbar.getBoundingClientRect().height;
-    // console.log(scrollHeight);
-    // console.log(navHeight);
-    if(scrollHeight>navHeight){
-        navbar.classList.add("fixed-nav")
-    }else{
-        navbar.classList.remove("fixed-nav")
-    }
-    if(scrollHeight>500){
-        topLink.classList.add("show-links")
-    }else{
-        topLink.classList.remove("show-links")
-    }
-});
-
-
-
-
-
